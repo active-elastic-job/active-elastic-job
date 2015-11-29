5f53faee9e9b6c2349d6e629fb403c1fc5ed84c5
Tawan Sierek <tawan.sierek@gmail.com>
2015-11-29 18:00:37 +0100

    Add .enqueue and test with mocked interface of aws client

--- lib/active_job/queue_adapters/rails_eb_job_adapter.rb
+++ lib/active_job/queue_adapters/rails_eb_job_adapter.rb
@@ -3,6 +3,14 @@ module ActiveJob
     class RailsEbJobAdapter
       class << self
         attr_writer :aws_client
+
+        def enqueue(job)
+          queue_url = @aws_client.get_queue_url(queue_name: job.queue_name.to_s)
+          @aws_client.send_message(
+            queue_url: queue_url,
+            message_body: job.serialize
+          )
+        end
       end
     end
   end
--- spec/active_job/queue_adapters/rails_eb_job_adapter_spec.rb
+++ spec/active_job/queue_adapters/rails_eb_job_adapter_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 require 'active_job'
 
 class TestJob < ActiveJob::Base
+  queue_as :high_priority
+
   def perform(test_arg)
     test_arg
   end
@@ -11,17 +13,33 @@ describe ActiveJob::QueueAdapters::RailsEbJobAdapter do
   subject(:adapter) { ActiveJob::QueueAdapters::RailsEbJobAdapter }
 
   let(:aws_client)  {
-    Aws::SQS::Client.new(stub_responses: true)
+    double("aws_client")
   }
   let(:job) { TestJob.new }
+  let(:queue_url) { "http://some_url" }
 
   before do
     adapter.aws_client = aws_client
   end
 
   describe ".enqueue" do
-    it "sends the serialized job as a message to an AWS SQS queue" do
+    it "selects the correct queue" do
+      expected_args = { queue_name: job.queue_name.to_s }
+      expect(aws_client).to receive(:get_queue_url).with(expected_args)
 
+      allow(aws_client).to receive(:send_message) { }
+      adapter.enqueue job
+    end
+
+    it "sends the serialized job as a message to an AWS SQS queue" do
+      allow(aws_client).to receive(:get_queue_url) { queue_url }
+      expected_args = {
+        queue_url: queue_url,
+        message_body: job.serialize
+      }
+      expect(aws_client).to receive(:send_message).with(expected_args)
+
+      adapter.enqueue job
     end
   end
 end
