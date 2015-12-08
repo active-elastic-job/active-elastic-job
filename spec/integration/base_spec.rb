152206765e9d4b2591a40dbf481cabebc75ae249
Tawan Sierek <tawan.sierek@gmail.com>
2015-12-08 22:00:41 +0100

    Deploy test versions using EB CLI

--- spec/integration/base_spec.rb
+++ spec/integration/base_spec.rb
@@ -1,17 +1,8 @@
 require 'spec_helper'
 
 describe "standard scenario" do
-  let(:version_label) { "app-#{SecureRandom.hex[0..6]}" }
-  let(:zip_file_name) { "#{version_label}.zip" }
-
   before do
-    build_gem do
-      zip_application(zip_file_name) do
-        s3_key = upload_application(zip_file_name)
-        create_application_version(s3_key, version_label)
-        deploy(version_label)
-      end
-    end
+    deploy
   end
 
   it "enqueues and consumes a job successfully" do
