5ad0fddadd583cc8ed665d7ee255042d571e0608
Tawan Sierek <tawan.sierek@gmail.com>
2015-12-20 21:57:46 +0100

    AWS SQS client is instantiated automatically

--- lib/active_job/queue_adapters/rails_eb_job_adapter.rb
+++ lib/active_job/queue_adapters/rails_eb_job_adapter.rb
@@ -2,13 +2,23 @@ module ActiveJob
   module QueueAdapters
     class RailsEbJobAdapter
       class << self
-        attr_writer :aws_client
+        attr_writer :aws_sqs_client
 
         def enqueue(job)
-          queue_url = @aws_client.get_queue_url(queue_name: job.queue_name.to_s)
-          @aws_client.send_message(
+          queue_url = aws_sqs_client.get_queue_url(queue_name: job.queue_name.to_s).queue_url
+          aws_sqs_client.send_message(
             queue_url: queue_url,
             message_body: job.serialize
+          )
+        end
+
+        private
+
+        def aws_sqs_client
+          @aws_sqs_client ||= Aws::SQS::Client.new(
+            access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+            secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+            region: ENV['AWS_REGION']
           )
         end
       end
--- spec/active_job/queue_adapters/rails_eb_job_adapter_spec.rb
+++ spec/active_job/queue_adapters/rails_eb_job_adapter_spec.rb
@@ -12,32 +12,36 @@ end
 describe ActiveJob::QueueAdapters::RailsEbJobAdapter do
   subject(:adapter) { ActiveJob::QueueAdapters::RailsEbJobAdapter }
 
-  let(:aws_client)  {
-    double("aws_client")
+  let(:aws_sqs_client)  {
+    double("aws_sqs_client")
   }
   let(:job) { TestJob.new }
   let(:queue_url) { "http://some_url" }
+  let(:queue_url_resp) { double("queue_url_resp") }
 
   before do
-    adapter.aws_client = aws_client
+    adapter.aws_sqs_client= aws_sqs_client
   end
 
   describe ".enqueue" do
     it "selects the correct queue" do
       expected_args = { queue_name: job.queue_name.to_s }
-      expect(aws_client).to receive(:get_queue_url).with(expected_args)
+      expect(aws_sqs_client).to receive(:get_queue_url).with(expected_args)
 
-      allow(aws_client).to receive(:send_message) { }
+      allow(aws_sqs_client).to receive(:get_queue_url) { queue_url_resp }
+      allow(queue_url_resp).to receive(:queue_url) { queue_url }
+      allow(aws_sqs_client).to receive(:send_message) { }
       adapter.enqueue job
     end
 
     it "sends the serialized job as a message to an AWS SQS queue" do
-      allow(aws_client).to receive(:get_queue_url) { queue_url }
+      allow(aws_sqs_client).to receive(:get_queue_url) { queue_url_resp }
+      allow(queue_url_resp).to receive(:queue_url) { queue_url }
       expected_args = {
         queue_url: queue_url,
         message_body: job.serialize
       }
-      expect(aws_client).to receive(:send_message).with(expected_args)
+      expect(aws_sqs_client).to receive(:send_message).with(expected_args)
 
       adapter.enqueue job
     end
