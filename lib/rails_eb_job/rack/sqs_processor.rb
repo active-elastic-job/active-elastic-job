0663c8c2c08a00044230594434692fc6ffebbf55
Tawan Sierek <tawan.sierek@gmail.com>
2015-12-30 16:48:28 +0100

    Intercept requests from EB SQS daemon
    Make `RailsEbJob::Rack::SqsProcessor` intercept and
    parse requests from the AWS SQS daemon. Further execute
    the serialized job by triggering the
    `ActiveJob::Base.execute` method
    
    Elastic beanstalk worker tier environments run
    a daemon in the background which poll a designated
    SQS queue for messages. A polled message is
    processes by the daemon which results in a http
    request to the application.

--- lib/rails_eb_job/rack/sqs_processor.rb
+++ lib/rails_eb_job/rack/sqs_processor.rb
@@ -9,6 +9,15 @@ module RailsEbJob
 
       def call(env)
         request = ActionDispatch::Request.new env
+        if request.headers['User-Agent'] =~ /aws-sqsd/
+          begin
+            job = JSON.load(request.body)
+            ActiveJob::Base.execute(job)
+          rescue StandardError => e
+            return ['500', {'Content-Type' => env['text/plain'] }, [e.message]]
+          end
+          return ['200', {'Content-Type' => 'application/json' }, [ '' ]]
+        end
         @app.call(env)
       end
     end
--- spec/rails_eb_job/rack/sqs_processor_spec.rb
+++ spec/rails_eb_job/rack/sqs_processor_spec.rb
@@ -1,19 +1,47 @@
 require 'spec_helper'
+require 'rack/mock'
 
 describe RailsEbJob::Rack::SqsProcessor do
-  let(:env) { double("env") }
+  let(:env) { Rack::MockRequest.env_for("http://example.com:8080/") }
   let(:app) { double("app") }
+  let(:original_response) { double("original_response") }
 
   subject(:sqs_processor) { RailsEbJob::Rack::SqsProcessor.new(app) }
 
   it "passes an ordinary request through" do
-    expect(app).to receive(:call).with(env)
-    sqs_processor.call(env)
+    expect(app).to receive(:call).with(env).and_return(original_response)
+    expect(sqs_processor.call(env)).to eq(original_response)
   end
 
-  context "when request is an SQS message" do
-      let(:env) {
-            
-      }
+  context "when request produced by EB SQS daemon" do
+    let(:job) { Helpers::TestJob.new('test') }
+
+    before do
+      env['HTTP_USER_AGENT'] = 'aws-sqsd/1.1'
+      env['rack.input'] = StringIO.new(JSON.dump(job.serialize))
+    end
+
+    it "intercepts request" do
+      expect(app).not_to receive(:call).with(env)
+      sqs_processor.call(env)
+    end
+
+    it "performs the job" do
+      expect(sqs_processor.call(env)[0]).to eq('200')
+    end
+
+    context "when job execution fails" do
+      let(:error_message)  { "intentional error" }
+
+      before do
+        allow(ActiveJob::Base).to receive(:execute).and_raise(error_message)
+      end
+
+      it "responds with a 500 error code" do
+        response = sqs_processor.call(env)
+        expect(response[0]).to eq('500')
+        expect(response[2][0]).to eq(error_message)
+      end
+    end
   end
 end
