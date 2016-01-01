c680e1f0a3149f0dcb7b705a1e61df919d1ac8e9
Tawan Sierek <tawan.sierek@gmail.com>
2016-01-01 22:57:03 +0100

    Add travis build

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
+[![Build Status](https://travis-ci.org/tawan/active-elastic-job.svg)](https://travis-ci.org/tawan/active-elastic-job)
 # active-elastic-job
--- bootstrap.sh
+++ bootstrap.sh
@@ -58,8 +58,5 @@ install nodejs nodejs
 install python-dev python-dev
 curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
 python get-pip.py
-pip install awsebcli
-
-install zip zip
-
-update-locale LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 LC_ALL=en_US.UTF-8
+#pip install --upgrade pip
+sudo pip install awsebcli
