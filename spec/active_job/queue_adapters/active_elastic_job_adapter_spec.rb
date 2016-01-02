f33bf4678da430e51e5b1a44ef940f95b2a4bf2f
Tawan Sierek <tawan.sierek@gmail.com>
2016-01-02 16:19:11 +0100

    Refactor spec
    Move common stubs to `before` block.

--- spec/active_job/queue_adapters/active_elastic_job_adapter_spec.rb
+++ spec/active_job/queue_adapters/active_elastic_job_adapter_spec.rb
@@ -14,6 +14,9 @@ describe ActiveJob::QueueAdapters::ActiveElasticJobAdapter do
   before do
     adapter.aws_sqs_client= aws_sqs_client
     allow(Rails.application).to receive(:secrets) { { secret_key_base: secret_key_base } }
+    allow(aws_sqs_client).to receive(:create_queue) { queue_url_resp }
+    allow(queue_url_resp).to receive(:queue_url) { queue_url }
+    allow(aws_sqs_client).to receive(:send_message) { }
   end
 
   describe ".enqueue" do
@@ -21,15 +24,10 @@ describe ActiveJob::QueueAdapters::ActiveElasticJobAdapter do
       expected_args = { queue_name: job.queue_name.to_s }
       expect(aws_sqs_client).to receive(:create_queue).with(expected_args)
 
-      allow(aws_sqs_client).to receive(:create_queue) { queue_url_resp }
-      allow(queue_url_resp).to receive(:queue_url) { queue_url }
-      allow(aws_sqs_client).to receive(:send_message) { }
       adapter.enqueue job
     end
 
     it "sends the serialized job as a message to an AWS SQS queue" do
-      allow(aws_sqs_client).to receive(:create_queue) { queue_url_resp }
-      allow(queue_url_resp).to receive(:queue_url) { queue_url }
       expect(aws_sqs_client).to receive(:send_message)
 
       adapter.enqueue job
