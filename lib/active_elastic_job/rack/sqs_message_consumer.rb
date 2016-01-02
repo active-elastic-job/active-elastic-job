14a69661b46c10263dbbd2cb5972185614eb9ccb
Tawan Sierek <tawan.sierek@gmail.com>
2016-01-02 23:58:48 +0100

    Add documentation of Rack middleware

--- lib/active_elastic_job/rack/sqs_message_consumer.rb
+++ lib/active_elastic_job/rack/sqs_message_consumer.rb
@@ -2,12 +2,22 @@ require "action_dispatch"
 
 module ActiveElasticJob
   module Rack
+    # This middleware intercepts requests which are sent by the SQS daemon
+    # running in {Amazon Elastic Beanstalk worker environments}[http://docs.aws.amazon.com/elasticbeanstalk/latest/dg/using-features-managing-env-tiers.html].
+    # It does this by looking at the +User-Agent+ header.
+    # Furthermore, it verifies the digest which is sent along with a legit SQS message,
+    # and passed as an HTTP header in the resulting request.
+    # The digest is based on Rails' +secrets.secret_key_base+.
+    # Therefore, the application running in the web environment, which generates
+    # the digest, and the application running in the worker
+    # environment, which verifies the digest, have to use the *same*
+    # +secrets.secret_key_base+ setting.
     class SqsMessageConsumer
-      def initialize(app)
+      def initialize(app) #:nodoc:
         @app = app
       end
 
-      def call(env)
+      def call(env) #:nodoc:
         request = ActionDispatch::Request.new env
         if request.headers['User-Agent'] =~ /aws-sqsd/
           begin
