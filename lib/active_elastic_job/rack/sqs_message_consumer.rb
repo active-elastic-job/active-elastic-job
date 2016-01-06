89bcd49aeaa57345e787f989104f6d09650bd984
Tawan Sierek <tawan.sierek@gmail.com>
2016-01-06 14:36:59 +0100

    Increase performance of user agent filtering
    Instead of matching the user agent of every request with
    a `Regexp` object, make a simple string comparison.
    Benchmark test showed a significant improvement.
    
    Simulating 3000000 requests with approximately
    1 in 30 requests matching the `aws-sqsd` user agent
    produced these results:
    
    user     system      total        real
    1.140000   0.000000   1.140000 (  1.151003)
    1.840000   0.000000   1.840000 (  1.839368)

--- lib/active_elastic_job/rack/sqs_message_consumer.rb
+++ lib/active_elastic_job/rack/sqs_message_consumer.rb
@@ -13,13 +13,15 @@ module ActiveElasticJob
     # environment, which verifies the digest, have to use the *same*
     # +secrets.secret_key_base+ setting.
     class SqsMessageConsumer
+      USER_AGENT_PREFIX = 'aws-sqsd'
+
       def initialize(app) #:nodoc:
         @app = app
       end
 
       def call(env) #:nodoc:
         request = ActionDispatch::Request.new env
-        if request.headers['User-Agent'] =~ /aws-sqsd/
+        if aws_sqsd?(request)
           begin
             verify(request)
             job = JSON.load(request.body)
@@ -41,6 +43,18 @@ module ActiveElasticJob
         verifier = ActiveElasticJob::MessageVerifier.new(secret_key_base)
         digest = request.headers['HTTP_X_AWS_SQSD_ATTR_MESSAGE_DIGEST']
         verifier.verify(request.body.string, digest)
+      end
+
+      def aws_sqsd?(request)
+        # we do not match against a Regexp
+        # in order to avoid performance penalties.
+        # Instead we make a simple string comparison.
+        # Benchmark runs showed an performance increase of
+        # up to 40%
+        current_user_agent = request.headers['User-Agent']
+        return (current_user_agent.present? &&
+          current_user_agent.size >= USER_AGENT_PREFIX.size &&
+          current_user_agent[0..(USER_AGENT_PREFIX.size - 1)] == USER_AGENT_PREFIX)
       end
     end
   end
