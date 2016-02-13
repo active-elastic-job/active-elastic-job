32f4be05b0ecdc3fb7ca12dc9facddd84a5e744b
Tawan Sierek <tawan.sierek@gmail.com>
2016-02-13 21:26:44 +0100

    Refactor spec helpers

--- spec/helpers.rb
+++ spec/helpers.rb
@@ -18,21 +18,6 @@ module Helpers
     end
   end
 
-  def deploy
-    Dir.chdir(root_dir) do
-      build_gem do
-        unpack_gem_into_vendor_dir
-        Dir.chdir("spec/integration/rails-app") do
-          [ WEB_ENV_NAME, WORKER_ENV_NAME ].each do |env|
-            unless system("eb deploy #{env}")
-              raise "Could not deploy application to environment #{env}"
-            end
-          end
-        end
-      end
-    end
-  end
-
   def aws_sqs_client
     Aws::SQS::Client.new(
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
@@ -41,76 +26,126 @@ module Helpers
     )
   end
 
-  def create_job(random_string, delay = 0)
-    resp = Net::HTTP.post_form(
-      URI("http://#{WEB_ENV_HOST}:#{WEB_ENV_PORT}/jobs"),
-      { "random_string" => random_string, "delay" => delay })
-    resp.value
-    resp
-  end
-
-  def fetch_random_strings
-    resp = JSON.load(
-      open("http://#{WEB_ENV_HOST}:#{WEB_ENV_PORT}/random_strings.json"))
-    resp.collect { |a| a["random_string"] }
-  end
-
-  def delete_random_string(random_string)
-    Net::HTTP.start(WEB_ENV_HOST, WEB_ENV_PORT) do |http|
-      http.delete("/random_strings/#{random_string}")
-    end
-  end
-
-  def create_random_string(random_string)
-    resp = Net::HTTP.post_form(
-      URI("http://#{WEB_ENV_HOST}:#{WEB_ENV_PORT}/random_strings"),
-      { "random_string" => random_string })
-    resp.value
-    resp
-  end
-
   def with_modified_env(options, &block)
     ClimateControl.modify(options, &block)
   end
 
-  private
+  class RailsApp
+    def initialize
+      @base_url = "http://#{WEB_ENV_HOST}:#{WEB_ENV_PORT}/"
+    end
 
-  def build_gem
-    unless system("gem build active-elastic-job.gemspec")
-      raise "Could not build gem package!"
-    end
-    begin
-      yield
-    ensure
-      FileUtils.rm("#{gem_package_name}.gem")
-    end
-  end
-
-  def unpack_gem_into_vendor_dir
-    target_dir = "spec/integration/rails-app/vendor/gems"
-    unless File.directory?(target_dir)
-      FileUtils.mkdir_p(target_dir)
-    end
-    unless system("gem unpack #{gem_package_name}.gem --target #{target_dir}")
-      raise "Could not unpack gem"
-    end
-    Dir.chdir(target_dir) do
-      unless system("rm -rf active_elastic_job-current") &&
-        system("mv #{gem_package_name} active_elastic_job-current")
-        raise "Could not move gem"
+    def deploy
+      build_gem
+      begin
+        unpack_gem_into_vendor_dir
+        deploy_to_environment(WEB_ENV_NAME)
+        deploy_to_environment(WORKER_ENV_NAME)
+      ensure
+        remove_gem
       end
     end
+
+    def create_delete_job(random_string, delay = 0)
+      resp = Net::HTTP.post_form(
+      URI("#{@base_url}jobs"),
+      { "random_string" => random_string, "delay" => delay })
+      resp.value
+      resp
+    end
+
+    def fetch_random_strings
+      resp = JSON.load(
+      open("#{@base_url}random_strings.json"))
+      resp.collect { |a| a["random_string"] }
+    end
+
+    def delete_random_string(random_string)
+      Net::HTTP.start(WEB_ENV_HOST, WEB_ENV_PORT) do |http|
+        http.delete("/random_strings/#{random_string}")
+      end
+    end
+
+    def create_random_string(random_string)
+      resp = Net::HTTP.post_form(
+      URI("#{@base_url}random_strings"),
+      { "random_string" => random_string })
+      resp.value
+      resp
+    end
+
+    private
+
+    def deploy_to_environment(env)
+      Dir.chdir("#{root_dir}/spec/integration/rails-app") do
+        unless system("eb deploy #{env}")
+          raise "Could not deploy application to environment #{env}"
+        end
+      end
+    end
+
+    def build_gem
+      sh(
+        "gem build active-elastic-job.gemspec",
+        "Could not build gem package!")
+    end
+
+    def remove_gem
+      sh("rm -rf #{gem_package_name}.gem", "Could not remove gem")
+    end
+
+    def unpack_gem_into_vendor_dir
+      target_dir = "#{root_dir}/spec/integration/rails-app/vendor/gems"
+      unless File.directory?(target_dir)
+        FileUtils.mkdir_p(target_dir)
+      end
+      sh(
+        "gem unpack #{gem_package_name}.gem --target #{target_dir}",
+        "Could not unpack gem")
+      sh(
+        "rm -rf #{target_dir}/active_elastic_job-current",
+        "Could not move gem")
+      sh(
+        "mv #{target_dir}/#{gem_package_name} #{target_dir}/active_elastic_job-current",
+        "Could not move gem")
+    end
+
+    def gem_package_name
+      "active_elastic_job-#{ActiveElasticJob::VERSION}"
+    end
+
+    def sh(command, error_msg)
+      Dir.chdir(root_dir) do
+        unless system(command)
+          raise error_msg
+        end
+      end
+    end
+
+    def root_dir
+      File.expand_path('../', File.dirname(__FILE__))
+    end
+  end
+end
+
+RSpec::Matchers.define :have_deleted do |expected|
+  match do |actual|
+    begin
+      Timeout::timeout(5) do
+        while(actual.fetch_random_strings.include?(expected)) do
+          sleep 1
+        end
+      end
+    rescue Timeout::Error
+      return false
+    end
+    return true
+  end
+  failure_message do |actual|
+    "Random string #{expected} has not been deleted within 5 seconds"
   end
 
-  def gem_package_name
-    "active_elastic_job-#{ActiveElasticJob::VERSION}"
-  end
-
-  def root_dir
-    File.expand_path('../', File.dirname(__FILE__))
-  end
-
-  def bucket
-    eb_client.create_storage_location.s3_bucket
+  failure_message_when_negated do |actual|
+    "Random string #{expected} has already been deleted"
   end
 end
--- spec/integration/standard_scenarios_spec.rb
+++ spec/integration/standard_scenarios_spec.rb
@@ -5,44 +5,33 @@ describe "standard scenarios", slow: true, deployed: true do
   let(:random_string) { SecureRandom.hex }
 
   before(:all) do
-    deploy
+    @rails_app = Helpers::RailsApp.new
+    @rails_app.deploy
   end
 
   it "posts a job to the queue and processes it" do
-    expect(fetch_random_strings).to_not include(random_string)
-    create_random_string(random_string)
-    expect(fetch_random_strings).to include(random_string)
-    create_job(random_string)
-    expect do
-      Timeout::timeout(5) do
-        while(fetch_random_strings.include?(random_string)) do
-          sleep 1
-        end
-      end
-    end.not_to raise_error
+    expect(@rails_app.fetch_random_strings).to_not include(random_string)
+    @rails_app.create_random_string(random_string)
+    expect(@rails_app.fetch_random_strings).to include(random_string)
+    @rails_app.create_delete_job(random_string)
+    expect(@rails_app).to have_deleted(random_string)
   end
 
   context "when job is scheduled for future processing" do
-    let(:delay) { 15 }
+    let(:delay) { 5 }
 
     it "waits until scheduled point in time" do
-      expect(fetch_random_strings).to_not include(random_string)
-      create_random_string(random_string)
-      expect(fetch_random_strings).to include(random_string)
-      create_job(random_string, delay)
+      expect(@rails_app.fetch_random_strings).to_not include(random_string)
+      @rails_app.create_random_string(random_string)
+      expect(@rails_app.fetch_random_strings).to include(random_string)
+      @rails_app.create_delete_job(random_string, delay)
 
       scheduled_at = Time.now + delay.seconds
       while ((Time.now + 1) < scheduled_at) do
-        expect(fetch_random_strings).to include(random_string)
+        expect(@rails_app.fetch_random_strings).to include(random_string)
         sleep 1
       end
-      expect do
-        Timeout::timeout(5) do
-          while(fetch_random_strings.include?(random_string)) do
-            sleep 1
-          end
-        end
-      end.not_to raise_error
+      expect(@rails_app).to have_deleted(random_string)
     end
   end
 end
