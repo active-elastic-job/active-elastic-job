2bfb7a3a64e046abacb750a433bed135dc546e79
Tawan Sierek <tawan.sierek@gmail.com>
2016-03-06 16:58:37 +0100

    Do not add middleware at all if disabled

--- lib/active_elastic_job/railtie.rb
+++ lib/active_elastic_job/railtie.rb
@@ -1,7 +1,10 @@
 module ActiveElasticJob
   class Railtie < Rails::Railtie
     initializer "active_elastic_job.insert_middleware" do |app|
-      app.config.middleware.use "ActiveElasticJob::Rack::SqsMessageConsumer"
+      disabled = ENV['DISABLE_SQS_CONSUMER']
+      if disabled == 'false' || disabled.nil?
+        app.config.middleware.use "ActiveElasticJob::Rack::SqsMessageConsumer"
+      end
     end
   end
 end
