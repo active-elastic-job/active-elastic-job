3cd2dc9171437273be04e188e0c3358e01bb9556
Joey Paris <joey@leadjig.com>
2021-01-26 15:41:18 -0500

    Don't redirect to HTTPS for the health check

--- spec/integration/rails-app-5.2/app/controllers/application_controller.rb
+++ spec/integration/rails-app-5.2/app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  def health_check
+    render json: {status: 200}, status: :ok
+  end
 end
