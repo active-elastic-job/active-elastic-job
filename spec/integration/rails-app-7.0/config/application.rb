7001b4602dc34d5f079893cc01d889199020b505
Joey Paris <joey@leadjig.com>
2022-01-07 13:29:39 -0500

    Initial attempt at supporting rails 7

---
+++ spec/integration/rails-app-7.0/config/application.rb
@@ -0,0 +1,29 @@
+require_relative "boot"
+
+require "rails/all"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module RailsApp
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 7.0
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
+
+    # Rails.application.config.active_record.sqlite3.represent_boolean_as_integer = true
+
+    config.active_job.queue_adapter = :active_elastic_job
+
+    config.force_ssl = true
+    config.ssl_options = { redirect: { exclude: -> request { request.path =~ /health/ } } }
+  end
+end
