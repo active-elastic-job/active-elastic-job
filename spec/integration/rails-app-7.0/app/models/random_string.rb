a664ae82eca2c7ce321ed80bda8d9a5890bf578e
joeyparis <mail@joeyparis.me>
2022-01-11 16:52:41 -0500

    Revert "Add Rails 7 support (#135)"
    This reverts commit 487d55c9e9875ca485e565ef44cf2630da399396.

--- spec/integration/rails-app-7.0/app/models/random_string.rb
+++
@@ -1,2 +0,0 @@
-class RandomString < ApplicationRecord
-end
--- spec/integration/rails-app-7.0/config/routes.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.routes.draw do
-  get '/health', to: 'application#health_check'
-  resources :random_strings
-  post 'jobs', to: 'jobs#create'
-end
