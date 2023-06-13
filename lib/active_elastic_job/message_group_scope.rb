455bc801ab2cf1634b213d180bd41a9e20bbe0c1
Guillermo Guerrero <guillermo@guerreroibarra.com>
2023-06-13 11:09:43 +0200

    Added more than one context.

--- lib/active_elastic_job/message_group_scope.rb
+++ lib/active_elastic_job/message_group_scope.rb
@@ -1,16 +1,15 @@
 module ActiveElasticJob
   class MessageGroupScope
-    @@message_group_suffix = Concurrent::ThreadLocalVar.new { nil }
+    @@message_group_suffix = Concurrent::ThreadLocalVar.new { [] }
 
     def self.message_group_suffix
-      @@message_group_suffix.value
+      @@message_group_suffix.value.last
     end
 
     def self.scope(message_group_suffix, &block)
-      @@message_group_suffix.value = message_group_suffix
+      @@message_group_suffix.value.push(message_group_suffix)
       output = block.call
-      @@message_group_suffix.value = nil
-      
+      @@message_group_suffix.value.pop
       output
     end
   end
